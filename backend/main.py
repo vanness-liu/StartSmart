bf8121b4b763e0a3a70f929544f6b89c72f43572
23004922@siswa.um.edu.my <23004922@siswa.um.edu.my>
2025-06-13 07:42:21 +0800

    resolve merge conflict

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,25 @@
 from fastapi import FastAPI
 from api import chat
 
+from fastapi.middleware.cors import CORSMiddleware
+from api.v1 import api_router
+from core.config import settings 
+
 app = FastAPI(
     title="StartSmart API"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost", "http://localhost:8080", "http://localhost:8081"], # Add Flutter dev origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(chat.router, prefix="/api", tags=["Chat"])
+app.include_router(api_router.router, prefix=settings.API_V1_STR)
+
+@app.get("/")
+async def root():
+    return {"message": f"Welcome to {settings.PROJECT_NAME}!"}
